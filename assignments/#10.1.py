6fb1744a654205fb5bfc71531ac2ecf6bbb98ce0
Kirill Blazhennov <kirill.blazhennov@gmail.com>
2018-02-26 22:27:09 -0800

    unit-tests example

--- assignments/#10.1.py
+++ assignments/#10.1.py
@@ -1,17 +1,34 @@
-
-#10.1 Split large number into hundreds, tens and units.
-
-
-print('This program split given large number into hundreds, tens and units.')
+"""
+This program split given large number into hundreds, tens and units.
+"""
 
 
-num = float(raw_input('Enter a number: '))
-
-i = 10
-while num > 0:
+def split_number(num):
+    result = []
+    i = 10
+    while num > 0:
         c = num % i
         num = num - c
         i = i*10
-        print(c)
+        result.append(c)
+    return list(reversed(result))
 
-              
+
+if __name__ == '__main__':
+    print(__doc__)
+
+    test_cases = [
+        (1, [1]),
+        (1025, [1000,0,20,5]),
+    ]
+
+    for testnum, test in enumerate(test_cases):
+        inp = test[0]
+        expected = test[1]
+        result = split_number(test[0])
+        print('test number: {}; input: {}; expected output: {}; actual output: {}'.format(
+            testnum, inp, expected, result))
+        if expected == result:
+            print('SUCCESS')
+        else:
+            print('ERROR')
